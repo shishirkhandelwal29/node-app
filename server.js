fa9aaa94ffe572361635d672d93c9fa1623b38a6
Shishir Khandelwal <38817206+shishirkhandelwal29@users.noreply.github.com>
2020-05-25 00:07:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Java Home App - version-10!!</h1> \n');
+  res.send('<h1 style="color:black;">Shishir CI CD using Jenkins - Completed finally!!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
